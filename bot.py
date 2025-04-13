103739d103a7fb5a9183a076096a5566e28f0849
VlasovIlya91 <101562469+VlasovIlya91@users.noreply.github.com>
2025-04-13 16:14:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import asyncio
+import os
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+# Загружаем переменные окружения
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 group_info = """Мы рады, что тебе интересны занятия волейболом! Вот, что мы можем предложить: 
 🔥 ОДИНЦОВО!
@@ -9,10 +15,7 @@ group_info = """Мы рады, что тебе интересны занятия
 📆 Понедельник, среда
 🏐 Уровень: средний-
 🧘‍♂️ Разминка, Офп, Техника, ±30 мин игра
-💸 1000 ₽/1000 ₽/1300 ₽
-пробное/по абонементу/разовое
 
-Тренер: Мария Стрекалова. КМС, 4-х кратная чемпионка России. 💪
 Бесплатная парковка, раздевалка и душ, большие ауты, высокие потолки!
 
 🥳 Только позитив, команда, новые знакомства и твой личный рост!
@@ -27,12 +30,15 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Спасибо за сообщение!")
 
 async def main():
-    app = ApplicationBuilder().token("8029397370:AAEFLQ_ZrEt-BMDSIHnE7u6zwwR1mJWI8os").build()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-    await app.run_polling()
+    await app.initialize()
+    await app.start()
+    print("Бот запущен. Ожидаем сообщения...")
+    await app.updater.start_polling()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
